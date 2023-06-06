ae05d254620f0760538aee8d1a8149592cfe4a46
Lawrence Karani <lawrencekarani@gmail.com>
2023-06-06 10:07:58 +0300

    initial commit

---
+++ src/main/java/com/example/esos/EsosApplication.java
@@ -0,0 +1,13 @@
+package com.example.esos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esos/EsosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
