b1daec9857c91ec44de30040b6e95a261f418d15
Lawrence Karani <lawrencekarani@gmail.com>
2023-06-06 19:13:16 +0300

    changed return type to ResponseEntity<> in IncidentService methods

--- src/main/java/com/example/esos/models/responses/GenericResponse.java
+++ src/main/java/com/example/esos/models/responses/GenericResponse.java
@@ -1,4 +1,8 @@
 package com.example.esos.models.responses;
 
 public class GenericResponse {
+
+    private String status;
+    private String message;
+
 }
--- src/main/java/com/example/esos/services/IncidentService.java
+++ src/main/java/com/example/esos/services/IncidentService.java
@@ -4,21 +4,23 @@ import com.example.esos.models.requests.IncidentCreate;
 import com.example.esos.models.requests.IncidentUpdate;
 import com.example.esos.models.responses.GenericResponse;
 import com.example.esos.models.responses.IncidentResponse;
+import org.springframework.http.ResponseEntity;
+
 
 import java.util.List;
 
 public interface IncidentService {
 
     //get incidents as per role
-    List<IncidentResponse> getIncidents();
+    ResponseEntity<IncidentResponse> getIncidents();
 
     //create incident
 
-    List<GenericResponse> createIncident (IncidentCreate incidentCreate);
+    ResponseEntity<GenericResponse> createIncident (IncidentCreate incidentCreate);
 
     //update incident
 
-    List<GenericResponse> updateIncident( IncidentUpdate incidentRequest);
+    ResponseEntity<GenericResponse>  updateIncident( IncidentUpdate incidentRequest);
 
 
 
